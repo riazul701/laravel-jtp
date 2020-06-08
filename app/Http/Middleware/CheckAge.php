35668175bfd387cf654dfa1fd298f4c5b9dbe162
Riazul Hasan <riazul701@gmail.com>
2020-06-08 17:38:39 +0600

    Code from 013_Laravel_Route_Groups.

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+//        return "middleware";
+        echo "Hello javaTpoint <br>";
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([], function() {
+    Route::get('/first', function() {
+        echo "first route";
+    });
+
+    Route::get('/second', function() {
+        echo "second route";
+    });
+
+    Route::get('/third', function() {
+        echo "third route";
+    });
+});
+
+Route::group(['prefix' => 'tutorial'], function() {
+    Route::get('/aws', function() {
+        echo "aws tutorial";
+    });
+
+    Route::get('/jira', function() {
+        echo "jira tutorial";
+    });
+
+    Route::get('/testing', function() {
+        echo "testing tutorial";
+    });
+});
+
+Route::middleware(['age'])->group(function() {
+    Route::get('/aws', function() {
+       echo "aws tutorial";
+    });
+
+    Route::get('/jira', function() {
+        echo "jira tutorial";
+    });
+
+    Route::get('/testing', function() {
+        echo "testing tutorial";
+    });
+});
+
+Route::name('admin.')->group(function() {
+    Route::get('users', function() {
+        return "admin.users";
+    })->name('users');
+});
