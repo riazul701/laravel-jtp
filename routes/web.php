ef8ba8b90ddab1bd91b684955c2481c29139abc8
Riazul Hasan <riazul701@gmail.com>
2020-06-08 16:04:36 +0600

    Code from 010_Routing_Parameters.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "This is a home page";
 });
+
+//Route::get('/about', function() {
+//    return "This is a about us page";
+//});
+//
+//Route::get('/contact', function() {
+//    return "This is a contact us page";
+//});
+//
+//Route::get('/post/{id}', function($id) {
+//    return "id number is : ".$id;
+//});
+//
+//// We can also pass the multiple parameters.
+//Route::get('/post/{id}/{name}', function($id,$name) {
+//    return "id number is : ".$id." ".$name;
+//});
+//
+//Route::get('user/{name?}', function($name=null) {
+//    return $name;
+//});
+//
+//Route::get('user/{name?}', function($name = 'himani') {
+//    return $name;
+//});
+//
+//Route::get('user/{name?}', function($name=null) {
+//    return $name;
+//})->where('name', '[a-zA-Z]+');
+//
+//Route::get('user/{id}', function($id = null) {
+//    return 'id is : '.$id;
+//})->where('id', '[0-9]+');
+//
+//Route::get('user/{id}/{name}', function($id, $name) {
+//    return "id is : ".$id.","."Name is : ".$name;
+//})->where(['id'=>'[0-9]+', 'name'=>'[a-zA-Z]+']);
+
+Route::get('user/{id}', function($id) {
+    return $id;
+});
+
+Route::get('post/{id}', function($id) {
+    return $id;
+});
+
