6d3b2bb15a1ff3b23e3da673bc28540959ee773c
Riazul Hasan <riazul701@gmail.com>
2020-06-08 20:35:48 +0600

    Code from 015_Routing_Controllers.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', 'PostController@index');
+
+Route::get('/post/{id}', 'PostController@index');
+
+Route::get('/user/{id}', 'UserController');
