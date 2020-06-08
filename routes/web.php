53c62e98c5f48884c6914462e157e858fe076a0f
Riazul Hasan <riazul701@gmail.com>
2020-06-08 22:31:45 +0600

    Code from 017_Controller_Middleware.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts', 'PostController@create')->middleware('check');
+
+//Route::get('posts', 'PostController@create');
+
+//Route::get('posts/{id}', 'PostController@show');
