b83b2ee7c562d44a0ae72e912f164c6df8ea2664
Riazul Hasan <riazul701@gmail.com>
2020-06-10 09:56:40 +0600

    Code from 027_Laravel_Database.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/insert', function() {
+    DB::insert('insert into posts(title,body) values(?,?)',['software developer','himanshu is a software developer']);
+});
+
+Route::get('/select', function() {
+    $results = DB::select('select * from posts where id=?',[1]);
+    foreach ($results as $posts) {
+        echo "title is : ".$posts->title;
+        echo "<br>";
+        echo "body is : ".$posts->body;
+    }
+});
+
+Route::get('/update', function() {
+    $updated = DB::update('update posts set title="software tester" where id=?',[1]);
+    return $updated;
+});
+
+Route::get('/delete', function() {
+    $deleted = DB::delete('delete from posts where id=?',[1]);
+    return $deleted;
+});
