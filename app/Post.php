28ce97f908999ec7c319e7cc6653f4105533b873
Riazul Hasan <riazul701@gmail.com>
2020-06-10 18:02:02 +0600

    Code from 028_Laravel_Eloquent.

---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+//    protected $fillable = ['title', 'body'];
+
+    protected $dates = ['deleted_at'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,83 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/read', function() {
+    $posts = Post::all();
+    foreach($posts as $post) {
+        echo $post->body;
+        echo '<br>';
+    }
+});
+
+//Route::get('/find', function() {
+//    $posts = Post::find(2);
+//    return $posts->title;
+//});
+
+//Route::get('/find', function() {
+//    $posts = Post::where('id',2)->first();
+//    return $posts;
+//});
+
+//Route::get('/find', function() {
+//    $posts = Post::where('id',1)->value('title');
+//    return $posts;
+//});
+
+Route::get('/insert', function() {
+    $post = new Post;
+    $post->title = 'QA Analyst';
+    $post->body = 'Nishka is a QA Analyst';
+    $post->save();
+});
+
+Route::get('/basicupdate', function() {
+    $post = Post::find(2);
+    $post->title = 'Graphic Designer';
+    $post->body = 'Tanisha is a graphic designer';
+    $post->save();
+});
+
+Route::get('/create', function() {
+    Post::create(['title' => 'Technical Content Writer', 'body' => 'Anusha is a technical content writer']);
+});
+
+Route::get('/update', function() {
+    Post::where('id',1)->update(['title' => 'Technical Content Writer 2', 'body' => 'Charu is a technical content writer']);
+});
+
+Route::get('/delete', function() {
+    $post = Post::find(1);
+    $post->delete();
+});
+
+//Route::get('/destroy', function() {
+//    Post::destroy(2);
+//});
+
+Route::get('/destroy', function() {
+    Post::destroy([3,4]);
+});
+
+Route::get('/delete1', function() {
+    Post::where('id', 5)->delete();
+});
+
+Route::get('/softdelete', function() {
+    Post::find(1)->delete();
+});
+
+Route::get('/readsofdelete', function() {
+    $post = Post::withTrashed()->where('id',1)->get();
+    return $post;
+});
+
+Route::get('/restore', function() {
+    Post::withTrashed()->where('id',1)->restore();
+});
+
+Route::get('/forcedelete', function() {
+    Post::onlyTrashed()->forceDelete();
 });
